a7fe36c8c5cd0004d8816230143758be8d80d886
Lambay mathéo <matheolambay@gmail.com>
2025-01-31 03:18:22 +0100

    add codex button

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import pygame
 from menu_manager import menuManager
 from menu.main_menu import mainMenu
 
-
 pygame.init()
 pygame.font.init()
 menu_manager = menuManager()
 menu_manager.push_menu(mainMenu(menu_manager.screen))
-
 
 run = True
 while run:
